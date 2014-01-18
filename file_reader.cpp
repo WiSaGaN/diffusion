f2f69a1625fd49e0d93e23a7bfff5ecfac286f07
WiSaGaN <wisagan@gmail.com>
2014-01-18 14:15:00 +0800

    Add more file part.

--- file_reader.cpp
+++ file_reader.cpp
@@ -1,11 +1,72 @@
 // Created on November 20, 2013 by Lu, Wangshan.
 #include <diffusion/factory.hpp>
+#include <fstream>
+#include <deque>
 namespace diffusion {
+extern const std::string kFileHeader;
 class FileReader : public Reader {
 public:
     FileReader(std::string const & file_name);
+    virtual ~FileReader();
     virtual bool can_read();
     virtual ByteBuffer read();
 private:
+    std::ifstream file_;
+    std::deque<ByteBuffer> data_queue_;
+    bool is_file_valid();
 };
+FileReader::FileReader(std::string const & file_name)
+    : file_(file_name) {
+    // TODO: Add file checking.
+    if (this->is_file_valid()) {
+    } else {
+    }
+}
+FileReader::~FileReader() {}
+bool FileReader::can_read() {
+    if (data_queue_.empty()) {
+        auto file_position_before_reading = file_.tellg();
+        ByteBuffer message_header(sizeof(Size));
+        if (!file_.read(message_header.data(), message_header.size())) {
+            // TODO: badbit throw read_fail.
+            file_.seekg(file_position_before_reading);
+            return false;
+        } else {
+            auto message_size = *reinterpret_cast<Size const *>(message_header.const_data());
+            ByteBuffer message(message_size);
+            if (!file_.read(message.data(), message.size())) {
+                // TODO: badbit throw read_fail.
+                file_.seekg(file_position_before_reading);
+                return false;
+            } else {
+                data_queue_.push_back(message);
+                return true;
+            }
+        }
+    } else {
+        return true;
+    }
+}
+ByteBuffer FileReader::read() {
+    if (this->can_read()) {
+        ByteBuffer data = data_queue_.front();
+        data_queue_.pop_front();
+        return data;
+    } else {
+        throw ErrorNoData();
+    }
+}
+bool FileReader::is_file_valid() {
+    auto file_header_length = kFileHeader.length();
+    std::string first_four_bytes(file_header_length, '0');
+    if (!file_.read(&first_four_bytes[0], file_header_length)) {
+        // TODO: badbit throw read_fail.
+        return false;
+    }
+    if (first_four_bytes == kFileHeader) {
+        return true;
+    } else {
+        return false;
+    }
+}
 } // namespace diffusion
--- file_writer.cpp
+++ file_writer.cpp
@@ -1,16 +1,29 @@
 // Created on November 29, 2013 by Lu, Wangshan.
+#include <fstream>
 #include <diffusion/factory.hpp>
 namespace diffusion {
+extern const std::string kFileHeader = "DFSN";
 class FileWriter : public Writer {
 public:
     FileWriter(std::string const & file_name);
+    virtual ~FileWriter();
     virtual void write(ByteBuffer const & data);
 private:
+    std::ofstream file_;
+    void write_file_header();
 };
-FileWriter::FileWriter(std::string const & file_name) {
-
+FileWriter::FileWriter(std::string const & file_name)
+    : file_(file_name) {
+    // TODO: Add file checking.
+    this->write_file_header();
+}
+FileWriter::~FileWriter() {
 }
 void FileWriter::write(ByteBuffer const & data) {
-
+    ByteBuffer data_serialization = prefix(data, data.size());
+    file_.write(data_serialization.const_data(), data_serialization.size());
+}
+void FileWriter::write_file_header() {
+    file_.write(kFileHeader.data(), kFileHeader.length());
 }
 } // namespace diffusion
