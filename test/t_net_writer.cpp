eb9c7c39583612a5025a0ca3d63d20c0bb8904a0
Wangshan Lu <wisagan@gmail.com>
2014-02-16 00:42:52 +0800

    More t_net_writer.

--- test/t_net_writer.cpp
+++ test/t_net_writer.cpp
@@ -12,5 +12,13 @@ int main(int argc, char * argv[]) {
     std::ifstream input_text_file(argv[2]);
     std::uint16_t listening_port = std::stoi(argv[1]);
     auto net_writer = std::shared_ptr<diffusion::Writer>(diffusion::create_network_writer(listening_port));
+    while (true) {
+        std::string current_line;
+        std::getline(input_text_file, current_line);
+        if (!input_text_file)
+            input_text_file.open(argv[2]);
+        net_writer->write(diffusion::ByteBuffer(current_line));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
     return 0;
 }
