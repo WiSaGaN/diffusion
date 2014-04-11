7919db5016a1adf42ffbeb08176db6f05dccf8ba
Wangshan Lu <wisagan@gmail.com>
2014-04-12 00:22:01 +0800

    More construction on net_writer.

--- src/net_writer.cpp
+++ src/net_writer.cpp
@@ -1,17 +1,45 @@
 // Created on November 19, 2013 by Lu, Wangshan.
 #include <diffusion/factory.hpp>
 #include <thread>
+#include <mutex>
+#include <queue>
 #include <boost/asio.hpp>
+#include <boost/concept_check.hpp>
 namespace diffusion {
 class Server {
 public:
     Server(boost::asio::io_service & input_io_service)
       : io_service_(input_io_service) {}
+    void post(ByteBuffer && data) {
+        if (!die_) {
+            std::lock_guard<std::mutex> lock(mutex_data_queue_);
+            data_queue_.push(std::move(data));
+        }
+    }
+    void run() {
+        while (!die_) {
+            ByteBuffer data(0);
+            {
+                std::lock_guard<std::mutex> lock(mutex_data_queue_);
+                if (!data_queue_.empty()) {
+                    data = data_queue_.front();
+                    data_queue_.pop();
+                }
+            }
+            if (data.size() > 0) {
+                std::cout.write(data.const_data(), data.size());
+                std::cout << std::endl;
+                // TODO: Send data to all connections.
+            }
+        }
+    }
     void shut_down() {
         die_ = true;
     }
 private:
-    bool die_;
+    volatile bool die_;
+    std::mutex mutex_data_queue_;
+    std::queue<ByteBuffer> data_queue_;
     boost::asio::io_service & io_service_;
 };
 class NetWriter : public Writer {
@@ -28,8 +56,7 @@ Writer * create_network_writer(std::uint16_t listening_port) {
     return new NetWriter(listening_port);
 }
 NetWriter::NetWriter(std::uint16_t listening_port)
-  : server_(io_service_) {
-
+  : server_(io_service_), server_thread_(&Server::run, &server_) {
 }
 NetWriter::~NetWriter() {
     server_.shut_down();
@@ -37,5 +64,6 @@ NetWriter::~NetWriter() {
 }
 void NetWriter::write(ByteBuffer const & data) {
     auto serialization = prefix(data, data.size());
+    server_.post(std::move(serialization));
 }
 } // namespace diffusion
