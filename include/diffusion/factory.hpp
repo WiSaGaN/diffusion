01402feab9c9d62b38c1f0b1d07bf9e42b4a1328
Wangshan Lu <wisagan@gmail.com>
2015-09-15 21:29:15 +0800

    Add vector char interface to writer.

--- include/diffusion/factory.hpp
+++ include/diffusion/factory.hpp
@@ -9,6 +9,7 @@ public:
     /// \brief Write data to medium. Medium can be shared memory, TCP/IP socket, or diffusion file.
     virtual void write(ByteBuffer const & data) = 0;
     virtual void write(std::vector<char> const &data) {}
+    virtual void write(char const *data, std::size_t size) {}
     /// \brief Virtual destructor for run-time polymorphism.
     virtual ~Writer() {}
 };
@@ -27,6 +28,7 @@ public:
     ///          To avoid throwing exception, one should always call can_read() before read().
     /// \return the available message in the medium.
     virtual ByteBuffer read() = 0;
+//    virtual std::vector<char> read() {}
     virtual void read(std::vector<char> &buffer) {}
     /// \brief Virtual destructor for run-time polymorphism.
     virtual ~Reader() {}
